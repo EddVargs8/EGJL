f5b2ccf62b99af5c527f4899fb12f01cfdfad888
EddVargs8 <l19290961@tectijuana.edu.mx>
2024-05-03 20:56:53 -0700

    configured settings for deployment

--- egjl/egjl/settings.py
+++ egjl/egjl/settings.py
@@ -15,15 +15,20 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import environ
+env = environ.Env()
+environ.Env.read_env() 
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#lqnfyj6i+!th@tk2tfjhd_eaas$7v*0bf3-cdni=hwac!+o2n'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -81,18 +86,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'egjl.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-
-#DATABASES = {
-#    'default': {
-#    'ENGINE': 'django.db.backends.sqlite3',
-#    'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -103,8 +97,16 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+'''
+
+# Render postgresql database 
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.parse(env('DATABASE_URL'))
 
 
+} 
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
